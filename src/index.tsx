93aed4a7ef830b189521f3c627233e7a514da580
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-03 17:04:00 +0300

    Adding Custom Environment Variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-} from "@apollo/client";
+import {ApolloProvider} from "@apollo/client";
 import {client} from './client'
 
 ReactDOM.render(
