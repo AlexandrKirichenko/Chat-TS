32cf6a80e1e3523ea3d8bda474a69b3b7dfbf81c
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-17 17:50:49 +0300

    reafact del old code

--- src/client.tsx
+++ src/client.tsx
@@ -31,41 +31,6 @@ export const client = new ApolloClient({
 
 
 
-// old
-// import { setContext } from '@apollo/client/link/context';
-//
-//
-// const httpLink = createHttpLink({
-//     uri: process.env.REACT_APP_MY_COOL_LINK,
-//     headers: {
-//         'access-token': localStorage.getItem('access-token') || null}
-// })
-//
-// const authLink = setContext((_, { headers }) => {
-//
-//     const token = localStorage.getItem('token') || null;
-//     console.log(token);
-//     return {
-//         headers: {
-//             ...headers,
-//             'access-token': token ? `Bearer ${token}` : "",
-//         }
-//     }
-// });
-//
-// export const client = new ApolloClient({
-//     cache: new InMemoryCache(),
-//     link: authLink.concat(httpLink)
-// });
-
-
-
-
-
-
-
-
-
 
 
 
