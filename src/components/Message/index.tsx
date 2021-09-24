a53d66b984ec4f040c32d5fd167b641c7b214b58
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-24 18:00:53 +0300

    start style componet

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,28 @@
+import classnames from 'classnames'
+import React, {useContext} from 'react'
+import {AuthContext} from '../../App'
+import Avatar from '../Avatar'
+import style from './Message.module.scss';
+import './Message.module.scss';
+
+interface DropdownPropsSet {
+    itsMe: boolean;
+}
+
+const Message: React.FC<DropdownPropsSet> = ({itsMe}) => {
+    const context = useContext(AuthContext);
+    if (context === null) {
+        return null;
+    }
+    const {user} = context;
+    
+    return (
+        <div className={style.wrapMessage}>
+            {user ? <Avatar value={user.avatar} nameAvatar={user.login} sizeAvatar={'small'} /> : null }
+            <div className={classnames(style.messageBlock, {[style.notMe] : !itsMe})}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque excepturi in ipsum, nihil nobis officiis optio quas, rerum sapiente tenetur totam vel veniam voluptas voluptatum. Laborum mollitia quam saepe?Lorem ipsum dolor sit amet, consectetur adipisicing elit. At deserunt doloremque dolores eius expedita iste iure maiores minima mollitia nostrum, nulla obcaecati officiis quasi reiciendis sequi sit totam, velit veritatis.</div>
+        </div>
+    
+    )
+}
+
+export default Message;
--- src/pages/ChatBlock/index.tsx
+++ src/pages/ChatBlock/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from './ChatBlock.module.scss';
+import Message from '../../components/Message'
 
 const ChatBlock: React.FC = () => {
-    return <div className={styles.chatBlock}>ChatBlock</div>
+    return(
+        
+        <div className={styles.chatBlock}>
+            <Message itsMe={true}/>
+        </div>
+    )
+    
 }
 
 export default ChatBlock;
