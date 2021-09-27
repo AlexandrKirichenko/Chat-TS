a2602a0106e863bd5f2dcfc68f5fd56c87bce560
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-27 09:27:42 +0300

    add id to schema

--- src/pages/ChatBlock/index.tsx
+++ src/pages/ChatBlock/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useContext} from 'react'
+import {AuthContext} from '../../App'
 import styles from './ChatBlock.module.scss';
 import Message from '../../components/Message'
 
 const ChatBlock: React.FC = () => {
+    const context = useContext(AuthContext);
+    
     return(
         
         <div className={styles.chatBlock}>
