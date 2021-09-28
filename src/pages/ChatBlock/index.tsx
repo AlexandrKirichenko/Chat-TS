0c4320061c8b187e09ca1af0e009f2bd57019172
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-28 10:35:37 +0300

    add props objects

--- src/pages/ChatBlock/index.tsx
+++ src/pages/ChatBlock/index.tsx
@@ -13,27 +13,31 @@ const ChatBlock: React.FC = () => {
         'tempore vitae?'
     const avatar2 ='https://i.natgeofe.com/n/9135ca87-0115-4a22-8caf-d1bdef97a814/75552_square.jpg'
     
+    const messageProps1 = {
+        key: 1,
+        itsMe: false,
+        login: 'login 1',
+        avatar: avatar,
+        description: description,
+        userId: 1,
+        id:'1'
+    }
+    const messageProps2 = {
+        key: 2,
+        itsMe: true,
+        login: 'login 1',
+        avatar: avatar2,
+        description: description2,
+        userId: 2,
+        id:'2'
+    }
+    
     return (
         <>
             <div className={styles.chatBlock}>
-                <Message
-                    key={1}
-                    itsMe={false}
-                    login={'login 1'}
-                    avatar={avatar}
-                    description={description}
-                    userId={1}
-                    id={'1'}
+                <Message {...messageProps1}
                 />
-                <Message
-                    key={2}
-                    itsMe={true}
-                    login={'login 2'}
-                    avatar={avatar2}
-                    description={description2}
-                    userId={2}
-                    id={'2'}
-                />
+                <Message {...messageProps2}/>
             </div>
         </>
     )
