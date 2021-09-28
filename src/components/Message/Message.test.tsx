a17bf65960d503d9f0da6cd2e5ae10bedeea1aea
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-28 15:10:50 +0300

    try new tests

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -2,7 +2,6 @@ import {render, screen} from '@testing-library/react'
 import React from 'react'
 import Message, {MessageProp} from './index'
 import ReactTestUtils from 'react-dom/test-utils';
-import TestRenderer from 'react-test-renderer';
 
 const messageText = 'Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam beatae consequatur ' +
     'cupiditate delectus dolore est id maxime mollitia necessitatibus nemo, neque optio quidem quis recusandae sint sunt ' +
@@ -18,39 +17,47 @@ const defaultProps: MessageProp = {
 }
 
 describe('Message component', () => {
-    it('Message rendering', () => {
-        const message = render(
-            <>
-                <Message
-                    {...defaultProps}
-                />
-            </>
-        )
-        expect(message).toMatchSnapshot()
-
-    })
-    it('dinamyc styles not adds when flag true', () => {
-            const props = {...defaultProps, itsMe: true}
-            render(
-                <Message
-                    {...props}
-                />
-            )
-            expect(screen.getByTestId('message__block')).toHaveClass('secondary')
-            expect(screen.getByTestId('message')).toHaveClass('secondary')
-        }
-    )
-    it('dinamyc styles not adds when flag false', () => {
-            const props = {...defaultProps, itsMe: false};
-            render(
-                <Message
-                    {...props}
-                />
-            )
-            expect(screen.getByTestId('message__block')).not.toHaveClass('secondary')
-            expect(screen.getByTestId('message')).not.toHaveClass('secondary')
-        }
-    )
+    it('renders correctly', () => {
+        const tree = ReactTestUtils
+            .create(<Message{...defaultProps} itsMe={true}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    
+    
+    // it('Message rendering', () => {
+    //     const message = render(
+    //         <>
+    //             <Message
+    //                 {...defaultProps}
+    //             />
+    //         </>
+    //     )
+    //     expect(message).toMatchSnapshot()
+    //
+    // })
+    // it('dinamyc styles not adds when flag true', () => {
+    //         render(
+    //             <Message
+    //                 {...defaultProps}
+    //                 itsMe={true}
+    //             />
+    //         )
+    //         expect(screen.getByTestId('message__block')).toHaveClass('secondary')
+    //         expect(screen.getByTestId('message')).toHaveClass('secondary')
+    //     }
+    // )
+    // it('dinamyc styles not adds when flag false', () => {
+    //         render(
+    //             <Message
+    //                 {...defaultProps}
+    //                 itsMe={true}
+    //             />
+    //         )
+    //         expect(screen.getByTestId('message__block')).not.toHaveClass('secondary')
+    //         expect(screen.getByTestId('message')).not.toHaveClass('secondary')
+    //     }
+    // )
 })
 
 
