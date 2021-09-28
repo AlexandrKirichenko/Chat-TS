9cbed090800c020a0f8cc9b8c4fa416325da519b
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-28 17:58:06 +0300

    refact

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -3,23 +3,17 @@ import Message, {MessageProp} from "./index";
 import {create, act} from "react-test-renderer";
 import TestRenderer from 'react-test-renderer';
 
-const messageText1 =
+const messageText =
     "Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam beatae consequatur " +
     "cupiditate delectus dolore est id maxime mollitia necessitatibus nemo, neque optio quidem quis recusandae sint sunt " +
     "tempore vitae?";
 
 
-const messageText2 =
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae cumque distinctio dolore dolorum earum," +
-    " excepturi facilis fugit harum hic id iste iure maxime nobis obcaecati officia provident quam reprehenderit, sequi " +
-    "tempore? Incidunt, reprehenderit, vel! Culpa deleniti enim eos est explicabo, facilis fuga fugiat inventore laboriosam " +
-    "magni quasi totam ullam!";
-
 const defaultProps1: MessageProp = {
     itsMe: false,
     login: "login 1",
     avatar: "",
-    messageText: messageText1,
+    messageText: messageText,
     userId: 1,
     id: "1",
 };
@@ -28,7 +22,7 @@ const defaultProps2: MessageProp = {
     itsMe: false,
     login: "login 2",
     avatar: "",
-    messageText: messageText2,
+    messageText: messageText,
     userId: 2,
     id: "2",
 };
