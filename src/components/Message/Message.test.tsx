cafbdcf2546e1e8ddb39f78aebdf7cefe6ecfc77
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-28 16:44:51 +0300

    testing

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -1,57 +1,75 @@
-import {render, screen} from '@testing-library/react'
-import React from 'react'
-import Message, {MessageProp} from './index'
+import React from "react";
+import Message, { MessageProp } from "./index";
+import { create, act } from "react-test-renderer";
 
-const messageText = 'Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam beatae consequatur ' +
-    'cupiditate delectus dolore est id maxime mollitia necessitatibus nemo, neque optio quidem quis recusandae sint sunt ' +
-    'tempore vitae?'
+const messageText =
+    "Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam beatae consequatur " +
+    "cupiditate delectus dolore est id maxime mollitia necessitatibus nemo, neque optio quidem quis recusandae sint sunt " +
+    "tempore vitae?";
 
 const defaultProps: MessageProp = {
     itsMe: false,
-    login: 'login 1',
-    avatar: 'https://avatarfiles.alphacoders.com/798/79894.jpg',
+    login: "login 1",
+    avatar: "",
     messageText,
     userId: 1,
-    id: '1'
-}
+    id: "1",
+};
 
-describe('Message component', () => {
-    it('Message rendering', () => {
-        const message = render(
-            <>
-                <Message
-                    {...defaultProps}
-                />
-            </>
-        )
-        expect(message).toMatchSnapshot()
-        
-    })
-    it('dinamyc styles not adds when flag true', () => {
-            const props = {...defaultProps, itsMe: true}
-            render(
-                <Message
-                    {...props}
-                />
-            )
-            expect(screen.getByTestId('message__block')).toHaveClass('secondary')
-            expect(screen.getByTestId('message')).toHaveClass('secondary')
-        }
-    )
-    it('dinamyc styles not adds when flag false', () => {
-            const props = {...defaultProps, itsMe: false};
-            render(
-                <Message
-                    {...props}
-                />
-            )
-            expect(screen.getByTestId('message__block')).not.toHaveClass('secondary')
-            expect(screen.getByTestId('message')).not.toHaveClass('secondary')
-        }
-    )
-})
+let container: any;
 
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
 
+afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+});
+
+describe("Message component", () => {
+    it("renders correctly", () => {
+        act(() => {
+            container = create(<Message {...defaultProps} itsMe={true} />).toJSON();
+            expect(container).toMatchSnapshot();
+        });
+    });
+    
+    // it('Message rendering', () => {
+    //     const message = render(
+    //         <>
+    //             <Message
+    //                 {...defaultProps}
+    //             />
+    //         </>
+    //     )
+    //     expect(message).toMatchSnapshot()
+    //
+    // })
+    // it('dinamyc styles not adds when flag true', () => {
+    //         render(
+    //             <Message
+    //                 {...defaultProps}
+    //                 itsMe={true}
+    //             />
+    //         )
+    //         expect(screen.getByTestId('message__block')).toHaveClass('secondary')
+    //         expect(screen.getByTestId('message')).toHaveClass('secondary')
+    //     }
+    // )
+    // it('dinamyc styles not adds when flag false', () => {
+    //         render(
+    //             <Message
+    //                 {...defaultProps}
+    //                 itsMe={true}
+    //             />
+    //         )
+    //         expect(screen.getByTestId('message__block')).not.toHaveClass('secondary')
+    //         expect(screen.getByTestId('message')).not.toHaveClass('secondary')
+    //     }
+    // )
+});
 
 
 
