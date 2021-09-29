575e1e5cde5e507b4b79ece79f00e2ecc13aebc8
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-29 12:11:01 +0300

    add ChatBlock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {useQuery} from '@apollo/client'
 import React, {useEffect, useState} from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, useHistory} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Layout from './components/Layout'
-import { PATH_LOGIN, PATH_REGISTRATION} from './config'
+import {PATH_CHAT_BLOCK, PATH_LOGIN, PATH_REGISTRATION} from './config'
+import ChatBlock from './pages/ChatBlock'
 import Login from './pages/Login'
 import Registration from './pages/Registration'
 import {ME} from './schemas'
@@ -31,12 +32,13 @@ function App() {
     const AuthContextData = {isAuthorized, setAutorized, user, setUser};
     
     const {data} = useQuery(ME);
-   
+    const history = useHistory();
     
     useEffect(() => {
         if (data) {
             setAutorized(true);
             setUser(data.me.user);
+            history.push(PATH_CHAT_BLOCK);
         }
     }, [data]);
     
@@ -51,7 +53,11 @@ function App() {
                             <Login/>
                         </Route>
                         <Route path={PATH_REGISTRATION}><Registration/></Route>
+                        <Route path={PATH_CHAT_BLOCK}>
+                            {isAuthorized ? <ChatBlock />: <div>You need to login</div>}
+                        </Route>
                     </Switch>
+                
                 </Layout>
             </AuthContext.Provider>
         </>
