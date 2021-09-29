21cf75112e69510f3b8fb214fbd4fb62bb0705fd
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-29 11:51:40 +0300

    dell src/pages/ChatBlock

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css'
 import Header from './components/Header'
 import Layout from './components/Layout'
 import {PATH_CHAT_BLOCK, PATH_LOGIN, PATH_REGISTRATION} from './config'
-import ChatBlock from './pages/ChatBlock'
 import Login from './pages/Login'
 import Registration from './pages/Registration'
 import {ME} from './schemas'
@@ -53,9 +52,6 @@ function App() {
                             <Login/>
                         </Route>
                         <Route path={PATH_REGISTRATION}><Registration/></Route>
-                        <Route path={PATH_CHAT_BLOCK}>
-                            {isAuthorized ? <ChatBlock />: <div>You need to login</div>}
-                        </Route>
                     </Switch>
     
                 </Layout>
