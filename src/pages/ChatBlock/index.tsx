d3eb0666e871189d919329d80d81436563302016
AlexandrKirichenko <kiri4enkoalexandr@gmail.com>
2021-09-30 21:35:06 +0300

    add refs and scroll

--- src/pages/ChatBlock/index.tsx
+++ src/pages/ChatBlock/index.tsx
@@ -1,5 +1,5 @@
-import {useQuery,useSubscription} from '@apollo/client'
-import React, {useContext,useRef} from 'react'
+import {useQuery, useSubscription} from '@apollo/client'
+import React, {useContext, useEffect, useRef} from 'react'
 import {AuthContext} from '../../App'
 import Button from '../../components/Button'
 import Message from '../../components/Message'
@@ -20,95 +20,81 @@ interface MessageProp {
     };
 }
 
+const scrollToRef = (ref: any) => window.scrollTo(0, ref.current.offsetTop)
+
 const ChatBlock: React.FC = () => {
     const {data, loading} = useQuery(GET_ALL_MESSAGES)
-    // const { data } = useSubscription(MESSAGE_ADDED)
-    const ScrollDemo = () => {
-        
-        const myRef = useRef<HTMLElement>(null)
-        const executeScroll = () => scrollToRef(myRef)
-        const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
-    // const { subscribeToMore, ...result }  = useQuery(GET_ALL_MESSAGES);
+    const myRef = useRef(null)
+    const executeScroll = () => scrollToRef(myRef)
+
     const messages: MessageProp[] = data ? data?.getAllMessages : null
     const context = useContext(AuthContext)
-    // const refToButton = useRef<HTMLElement>(null)
-    // const refToChat = useRef<HTMLElement>(null)
-
-    // ----------------------
-    // const { subscribeToMore, ...result } = useQuery(GET_ALL_MESSAGES);
- 
-// General scroll to element function
-    
-   
-    
-    // -------------------
+    useEffect(() => {
+        executeScroll()
+    }, [messages]);
     if (context === null) {
         return null
     }
-    const {user,isAuthorized} = context
+    const {user, isAuthorized} = context
     if (user === null) {
         return null
     }
-    
     const current = true
-        executeScroll();
-    
-    return (
-        
-        isAuthorized ?
-        <>
-            <div className={styles.wrapper} >
-                <div className={styles.sidebar}>
-                    <div className={styles.control}>
-                        Rooms
-                        <button className={styles.plus}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
-                                <path
-                                    d="M236.235 -50.5098L231.529 -50.5098L231.529 -44.8627L225.882 -44.8627L225.882 -40.1569L231.529 -40.1569L231.529 -34.5098L236.235 -34.5098L236.235 -40.1569L241.882 -40.1569L241.882 -44.8627L236.235 -44.8627L236.235 -50.5098Z"
-                                    transform="translate(-225.88235473632812, 50.509803771972656)"/>
-                            </svg>
-                        </button>
-                    </div>
-                    <div className={styles.rooms}>
-                        <div className={`${styles.room} ${current && styles.current}`}>
-                            General
+    return (isAuthorized ?
+            <>
+                <div className={styles.wrapper}>
+                    <div className={styles.sidebar}>
+                        <div className={styles.control}>
+                            Rooms
+                            <button className={styles.plus}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+                                    <path
+                                        d="M236.235 -50.5098L231.529 -50.5098L231.529 -44.8627L225.882 -44.8627L225.882 -40.1569L231.529 -40.1569L231.529 -34.5098L236.235 -34.5098L236.235 -40.1569L241.882 -40.1569L241.882 -44.8627L236.235 -44.8627L236.235 -50.5098Z"
+                                        transform="translate(-225.88235473632812, 50.509803771972656)"/>
+                                </svg>
+                            </button>
                         </div>
-                        <div className={styles.room}>
-                            Room 1
-                        </div>
-                        <div className={styles.room}>
-                            Room 2
+                        <div className={styles.rooms}>
+                            <div className={`${styles.room} ${current && styles.current}`}>
+                                General
+                            </div>
+                            <div className={styles.room}>
+                                Room 1
+                            </div>
+                            <div className={styles.room}>
+                                Room 2
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.chatBlock}>
-                    {messages ? messages.map(msg => (
-                            <>
-                                <div className={styles.sidebar}></div>
+                    <div className={styles.chatBlock}>
+                        {messages ? messages.map(msg => (
+                                <>
+                                    <div className={styles.sidebar}></div>
+                                    
+                                    <Message key={msg.id}
+                                             itsMe={msg.userId === Number(user?.id)}
+                                             messageText={msg.description}
+                                             login={msg.user.login}
+                                             userId={msg.userId}
+                                             avatar={msg.user.avatar}
+                                             id={msg.id}
+                                    />
                                 
-                                <Message key={msg.id}
-                                         itsMe={msg.userId === Number(user?.id)}
-                                         messageText={msg.description}
-                                         login={msg.user.login}
-                                         userId={msg.userId}
-                                         avatar={msg.user.avatar}
-                                         id={msg.id}
-                                />
-                            
-                            </>
-                        )
-                    ) : null}
-                        <div  ref={myRef} className={styles.messageForm} >
+                                </>
+                            )
+                        ) : null}
+                        <div ref={myRef} className={styles.messageForm}>
                             <textarea name="textarea" placeholder="Type your message"></textarea>
                             <Button type={'submit'} color={'primary'}
                                     size={'mediumChat'}> Send </Button>
                         </div>
+                    </div>
                 </div>
-            </div>
-        </> : <div> You should login </div>
+            </> : <div> You should login </div>
     )
     
-}}
+    
+}
 
 export default ChatBlock;
 
