be1106f7e53ddc17a96c14923e65c872ca8b482e
VladimirDiamandi <vladdiamandi@gmail.com>
2021-10-04 13:35:42 +0300

    refactored subscr

--- src/client.tsx
+++ src/client.tsx
@@ -1,113 +1,55 @@
-import {ApolloClient, InMemoryCache,createHttpLink} from '@apollo/client'
-import {setContext} from '@apollo/client/link/context'
-import {LS_TOKEN_KEY} from "./config";
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { split, HttpLink } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloClient, InMemoryCache, ApolloLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { LS_TOKEN_KEY } from "./config";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
 
-const token = localStorage.getItem(LS_TOKEN_KEY) || null;
- const wsLink = new WebSocketLink({
-    
+const wsLink = new WebSocketLink({
     uri: "wss://test-chat-be.herokuapp.com/graphql",
     options: {
-         reconnect: true
+        reconnect: true,
+        connectionParams: () => ({
+            "access-token": localStorage.getItem(LS_TOKEN_KEY) || null,
+        }),
     },
+});
 
- });
- 
 const httpLink = new HttpLink({
     uri: process.env.REACT_APP_MY_COOL_LINK,
-})
+});
 
 const splitLink = split(
     ({ query }) => {
         const definition = getMainDefinition(query);
         return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
         );
     },
     wsLink,
-    httpLink,
+    httpLink
 );
 
-const authLink = setContext((_, { headers }) => {
+const authLink = new ApolloLink((operation, forward) => {
     const token = localStorage.getItem(LS_TOKEN_KEY) || null;
-    return {
-        headers: {
-            ...headers,
-            'access-token': token ? `${token}` : "",
-        }
-    }
+    debugger;
+    operation.setContext(({ context = {}, headers = {} }) => {
+        return {
+            context: {
+                ...context,
+                "access-token": token ? `${token}` : "",
+            },
+            headers: {
+                ...headers,
+                "access-token": token ? `${token}` : "",
+            },
+        };
+    });
+    return forward(operation);
 });
 
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: authLink.concat(splitLink),
 });
-
-
-
-
-
-
-
-
-
-
-
-// import {ApolloClient, InMemoryCache,createHttpLink} from '@apollo/client'
-// import {setContext} from '@apollo/client/link/context'
-// import {LS_TOKEN_KEY} from "./config";
-// import { WebSocketLink } from '@apollo/client/link/ws';
-// import { split, HttpLink } from '@apollo/client';
-// import { getMainDefinition } from '@apollo/client/utilities';
-//
-//
-// const wsLink = new WebSocketLink({
-//
-//     uri: "wss://test-chat-be.herokuapp.com/graphql",
-//     options: {
-//         reconnect: true
-//     },
-//
-// });
-//
-// const httpLink = new HttpLink({
-//     uri: process.env.REACT_APP_MY_COOL_LINK,
-// })
-//
-// const authLink = setContext((_, { headers }) => {
-//     const token = localStorage.getItem(LS_TOKEN_KEY) || null;
-//     return {
-//         headers: {
-//             ...headers,
-//             'access-token': token ? `${token}` : "",
-//         }
-//     }
-// });
-//
-//
-// const wrappedHHTPLink = authLink.concat(httpLink);
-// const cache = new InMemoryCache();
-//
-// const splitLink = split(
-//     ({ query }) => {
-//         const definition = getMainDefinition(query);
-//         return (
-//             definition.kind === 'OperationDefinition' &&
-//             definition.operation === 'subscription'
-//         );
-//     },
-//     wsLink,
-//     wrappedHHTPLink,
-// );
-//
-//
-// export const client = new ApolloClient({
-//     cache,
-//     link : splitLink
-// });
-
-
-
