554ab677a4bc868c37c153071bd23a91f134a726
VladimirDiamandi <vladdiamandi@gmail.com>
2021-10-08 10:52:28 +0300

    fix erorrs

--- src/components/Rooms/index.tsx
+++ src/components/Rooms/index.tsx
@@ -1,4 +1,4 @@
-import {useApolloClient,useQuery} from '@apollo/client'
+import {useApolloClient, useQuery} from '@apollo/client'
 import classnames from 'classnames'
 import React, {useEffect, useMemo, useState} from 'react'
 import styles from './Rooms.module.scss'
@@ -9,22 +9,25 @@ import Room from '../Room'
 
 
 interface RoomsItem {
-  selectedRoomId: number|null;
-  changeSelectedRoomId: (values: number|null) => void;
+  id: number;
+  name: string;
 }
 
-// interface RoomProps {
-//   id: number;
-//   name: string;
-// }
+interface RoomProps {
+  
+  selectedRoomId: number | null;
+  changeSelectedRoomId: (values: number | null) => void;
+}
 
 
-const Rooms:React.FC<RoomsItem> =( { selectedRoomId,changeSelectedRoomId }) => {
-  const {data: allRooms} = useQuery(GET_ALL_CONVERSATIONS, {onCompleted:
+const Rooms: React.FC<RoomProps> = ({selectedRoomId, changeSelectedRoomId}) => {
+  const {data: allRooms} = useQuery(GET_ALL_CONVERSATIONS, {
+    onCompleted:
       (allRooms) => {
-        setRooms([...allRooms.getAllConversations])}
+        setRooms([...allRooms.getAllConversations])
+      }
   });
-  const [room, setRooms] = useState<RoomsItem[]>([]);
+  const [rooms, setRooms] = useState<RoomsItem[]>([]);
   const client = useApolloClient()
   let sub: any;
   
@@ -33,7 +36,6 @@ const Rooms:React.FC<RoomsItem> =( { selectedRoomId,changeSelectedRoomId }) => {
   //   console.log('Clicked')
   // }
   
-
   
   useEffect(() => {
     console.log('subscr1')
@@ -57,8 +59,8 @@ const Rooms:React.FC<RoomsItem> =( { selectedRoomId,changeSelectedRoomId }) => {
       <div className={styles.rooms}>
         {
           // rooms.map(rooms =>  <div key={rooms.id} className={classnames(styles.room,styles.current:[current] )} onClick={changeSelectedRoomId(rooms.id)}>{rooms.name}</div>
-          room.map(room => <Room key={room.id} onClick={()=>changeSelectedRoomId(room.id)}
-                                   isActive={room.id === selectedRoomId}>
+          rooms.map(room => <Room key={room.id} onClick={() => changeSelectedRoomId(room.id)}
+                                  isActive={room.id === selectedRoomId}>
               {room.name}
             </Room>
           )}
