7049b5a8b8c30818522d477d8b66e0680ca8d987
VladimirDiamandi <vladdiamandi@gmail.com>
2021-10-15 10:25:23 +0300

    add store

---
+++ src/store/authSlice.ts
@@ -0,0 +1,15 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+interface authSlice {
+
+}
+
+const authSlice = createSlice({
+    name: 'autorization',
+    initialState: {
+      isAuthorized: false
+    },
+    reducers: {
+    
+    }
+});
